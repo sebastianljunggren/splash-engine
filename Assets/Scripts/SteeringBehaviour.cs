e62a4c43de25ff0a40be2ace3948494b3d4da8d1
Andreas Lundquist <andreas.lunkan@gmail.com>
2014-01-29 14:30:45 +0100

    Simple AI just following the player.

---
+++ Assets/Scripts/SteeringBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringBehaviour : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	private Transform transformCache;
+	private CharacterController charController;
+
+	// Use this for initialization
+	void Start () {
+		transformCache = transform;
+		charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target.position);
+
+		Vector3 forward = transformCache.TransformDirection(Vector3.forward);
+		charController.SimpleMove(forward * speed);
+	}
+}

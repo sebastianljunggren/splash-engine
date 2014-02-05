318b9f344fe80f50dff78750409a34b17f326e0b
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-02-05 10:16:04 +0100

    Spawn a lot of particles in one shot.

--- Assets/Scripts/FluidBehaviour.cs
+++ Assets/Scripts/FluidBehaviour.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class FluidBehaviour : MonoBehaviour {
 
 	public List<FluidParticle> Particles;
-	private List<Vector3> particleRow;
+	private List<Vector3> ParticleRow;
 	private const float LAYER_OFFSET = 0.01f;
 	public int LayersInShot = 10;
 
@@ -13,16 +13,16 @@ public class FluidBehaviour : MonoBehaviour {
 		int layerCount = 4;
 		int layerMultiplier = 3;
 		int particlesInLayer = 1;
-		particleRow = new List<Vector3>();
+		ParticleRow = new List<Vector3>();
 		particlesInLayer = 1;
 		int particle = 0;
 		for (int layer = 0; layer < layerCount; layer++) {
 			for (int i = 0; i < particlesInLayer; i++, particle++) {
 				float rotation = (2 * Mathf.PI / particlesInLayer) * i;
-				particleRow.Add(new Vector3(
+				ParticleRow.Add(new Vector3(
 					Mathf.Cos(rotation) * LAYER_OFFSET * layer,
-					Mathf.Sin(rotation) * LAYER_OFFSET * layer,
-					0
+					Mathf.Sin(rotation) * LAYER_OFFSET * layer + 0.5f,
+					0.5f
 				));
 			}
 			particlesInLayer *= layerMultiplier;
@@ -36,15 +36,18 @@ public class FluidBehaviour : MonoBehaviour {
 	void OnDrawGizmos () {
 		Gizmos.color = Color.red;
 		foreach (FluidParticle p in Particles) {
-			Gizmos.DrawSphere(p.Position, 0.01f);
+			Gizmos.DrawSphere(p.Position, 0.005f);
 		}
 	}
 
 	public void ShootFluid (Transform transform) {
-		Vector3 v = particleRow[0];
-		FluidParticle p = new FluidParticle(transform.TransformPoint(v), transform.forward);
-		Particles.Add(p);
-		Debug.Log("New particle added. Position: " + p.Position + ", Velocity: " + p.Velocity + ".");
-
+		for (int i = 0; i < LayersInShot; i++) {
+			foreach(Vector3 v in ParticleRow) {
+				FluidParticle p = new FluidParticle(
+					transform.TransformPoint(v + new Vector3(0, 0 , LAYER_OFFSET * i)),
+					transform.forward);
+				Particles.Add(p);
+			}
+		}
 	}
 }

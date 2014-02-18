345a47f99c8032da474c61d6582fe3e44b7abd51
Isak Stråvik <istravik@gmail.com>
2014-02-18 10:00:47 +0100

    The spread on a flat object is now working properly.

--- Assets/Scripts/Fire/Propagation/FireEventManager.cs
+++ Assets/Scripts/Fire/Propagation/FireEventManager.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class FireEventManager {
-    public delegate void FireSpreadEvent(Vector3 position, float radius, int damage);
-    public static event FireSpreadEvent FireSpread;
+    //public delegate void FireSpreadEvent(Vector3 position, float radius, int damage);
+    //public static event FireSpreadEvent FireSpread;
 
     public delegate void SpawnFireEvent(Vector3 position);
     public static event SpawnFireEvent SpawnFire;
@@ -12,11 +12,11 @@ public class FireEventManager {
 
     }
 
-    public static void FireAt(Vector3 position, float radius) {
-        if (FireSpread != null) {
-            FireSpread(position, radius, 2);
-        }
-    }
+    //public static void FireAt(Vector3 position, float radius) {
+    //    if (FireSpread != null) {
+    //        FireSpread(position, radius, 2);
+    //    }
+    //}
 
     public static void SpawnFireAt(Vector3 position) {
         if (SpawnFire != null) {

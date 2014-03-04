b35a3a163e5da119bdc62a1deeb03920efedaae6
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-03-04 10:08:00 +0100

    Some fixes to shooting a stream of particles.

--- Assets/Scripts/FluidShooterBehaviour.cs
+++ Assets/Scripts/FluidShooterBehaviour.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class FluidShooterBehaviour : MonoBehaviour {
 
 	public FluidBehaviour water;
+    private bool shootStream = false;
 
 	// Update is called once per frame
-	void Update() {
+	void FixedUpdate() {
+        shootStream = !shootStream;
 		if (Input.GetButtonDown ("Fire1")) {
 			water.ShootFluidParticles(this.transform);
 		}
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButton("Fire2") && shootStream)
         {
             water.ShootFluidParticle(this.transform);
         }

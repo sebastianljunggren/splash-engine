07fced0069ec388a7e09d36d914a2432cd2781f8
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2014-03-09 12:35:23 +0100

    Clean up and refactoring.

--- Assets/Scripts/FluidBehaviour.cs
+++ Assets/Scripts/FluidBehaviour.cs
@@ -1,43 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class FluidBehaviour : MonoBehaviour {
+public class FluidBehaviour : MonoBehaviour
+{
 
-	public List<FluidParticle> Particles;
-    public List<FluidParticle> NewParticles;
-	private List<Vector3> ParticleShot;
-	private const float LAYER_OFFSET = 0.25f;
-	private const float GRAVITY = 0.005f;
-    private const int PARTICLE_DIAMETER_IN_SHOT = 5;
-    private const float REST_DENSITY = 100f;
-    private const float RELAXATION_CONSTANT = 0.0002f;
-	private int ParticleCount = 0;
-    private const float NEIGHBOUR_DISTANCE = LAYER_OFFSET;
-	private const float PARTICLE_ELASTICITY = 0.001f;
-    private const int SOLVER_ITERATIONS = 4;
+    public List<FluidParticle> Particles { get; set; }
+    private List<FluidParticle> newParticles;
+    private List<Vector3> particleShot;
+    private const float ParticleDistance = 0.25f;
+    private readonly Vector3 Gravity = new Vector3(0, 0.005f, 0);
+    private const int ShotParticleDiameter = 5;
+    private const float RestDensity = 100f;
+    private const float RelaxationConstant = 0.0002f;
+    private int particleCount = 0;
+    private const float NeighbourDistance = ParticleDistance;
+    private const float ParticleElasticity = 0.001f;
+    private const int SolverIterations = 4;
     private System.Random random = new System.Random();
-    private const float PARTICLE_VELOCITY_SCALING = 0.25f;
+    private const float ParticleVelocityScaling = 0.25f;
+    private const bool RemoveExpiredParticles = true;
 
-	public FluidBehaviour() {
-		this.Particles = new List<FluidParticle>();
-        this.NewParticles = new List<FluidParticle>();
-		this.ParticleShot = new List<Vector3>();
-        float radius = 0.5f * LAYER_OFFSET * PARTICLE_DIAMETER_IN_SHOT;
+    public FluidBehaviour()
+    {
+        this.Particles = new List<FluidParticle>();
+        this.newParticles = new List<FluidParticle>();
+        this.particleShot = new List<Vector3>();
+        float radius = 0.5f * ParticleDistance * ShotParticleDiameter;
         //  Create cube with points
-		for (int x = 0; x < PARTICLE_DIAMETER_IN_SHOT; x++) {
-            for (int y = 0; y < PARTICLE_DIAMETER_IN_SHOT; y++)
+        for (int x = 0; x < ShotParticleDiameter; x++)
+        {
+            for (int y = 0; y < ShotParticleDiameter; y++)
             {
-                for (int z = 0; z < PARTICLE_DIAMETER_IN_SHOT; z++)
+                for (int z = 0; z < ShotParticleDiameter; z++)
                 {
-                    Vector3 v = new Vector3(x * LAYER_OFFSET - radius, y * LAYER_OFFSET - radius, z * LAYER_OFFSET);
-                    this.ParticleShot.Add(v);
+                    Vector3 v = new Vector3(x * ParticleDistance - radius, y * ParticleDistance - radius, z * ParticleDistance);
+                    this.particleShot.Add(v);
                 }
 
             }
-		}
+        }
         Vector3 center = new Vector3(0, 0, radius);
         // Create sphere shape for shot by removing points
-       this.ParticleShot.RemoveAll(point => Vector3.Distance(point, center) > (radius + 0.1f * LAYER_OFFSET) );
+        this.particleShot.RemoveAll(point => Vector3.Distance(point, center) > (radius + 0.1f * ParticleDistance));
     }
 
     void Start()
@@ -45,43 +49,56 @@ public class FluidBehaviour : MonoBehaviour {
         Screen.showCursor = false;
     }
 
-	void FixedUpdate () {
-		// Remove all particles that have fallen below the level.
-		Particles.RemoveAll(p => p.ToBeRemoved || (p.Position.y < -1) || p.Expiry <= 0);
+    void FixedUpdate()
+    {
+        // Remove all particles that have fallen too low, have expired or hit fire
+        if (RemoveExpiredParticles)
+        {
+            Particles.RemoveAll(p => p.ToBeRemoved || (p.Position.y < -1) || p.Expiry <= 0);
+        }
+        else
+        {
+            Particles.RemoveAll(p => p.ToBeRemoved || (p.Position.y < -1));
+        }
 
         // Add new particles
-        Particles.AddRange(NewParticles);
-        NewParticles.Clear();
+        Particles.AddRange(newParticles);
+        newParticles.Clear();
 
-		int newParticleCount = Particles.Count;
-		if (newParticleCount != ParticleCount) {
-			ParticleCount = newParticleCount;
-            Debug.Log(ParticleCount + " fluid particles.");
-		}
+        int newParticleCount = Particles.Count;
+        if (newParticleCount != particleCount)
+        {
+            particleCount = newParticleCount;
+            Debug.Log(particleCount + " fluid particles.");
+        }
 
-		// Predict the positions of the particles
+        // Predict the positions of the particles
         // Lines 1-4 from the algorithm in the paper
-		foreach (FluidParticle p in Particles) {
-			// Apply gravity
-			p.Velocity.y -= GRAVITY;
+        foreach (FluidParticle p in Particles)
+        {
+            // Apply gravity
+            p.Velocity -= Gravity;
 
-			Vector3 newPosition = p.Position + p.Velocity;
+            Vector3 newPosition = p.Position + p.Velocity;
             RaycastHit hit = new RaycastHit();
-			if (Physics.Linecast(p.Position, newPosition + LAYER_OFFSET * (newPosition - p.Position).normalized, out hit)) {
-				if (hit.normal.y != 0) {
-					p.Velocity.y += GRAVITY;
-				}
-				p.Velocity = Vector3.Reflect(p.Velocity * PARTICLE_ELASTICITY, hit.normal);
-				newPosition = p.Position + p.Velocity;
+            if (Physics.Linecast(p.Position, newPosition + ParticleDistance * (newPosition - p.Position).normalized, out hit))
+            {
+                if (hit.normal.y != 0)
+                {
+                    p.Velocity += Gravity;
+                }
+                p.Velocity = Vector3.Reflect(p.Velocity * ParticleElasticity, hit.normal);
+                newPosition = p.Position + p.Velocity;
 
                 FireCell fireCell = hit.collider.GetComponent<FireCell>();
-                if (fireCell != null) {
+                if (fireCell != null)
+                {
                     fireCell.Water();
                     p.ToBeRemoved = true;
                 }
-			}
-			p.PredictedPosition = newPosition;
-		}
+            }
+            p.PredictedPosition = newPosition;
+        }
 
         // Find neighbours of each particle
         // Lines 5-7 from the algorithm in the paper
@@ -93,7 +110,7 @@ public class FluidBehaviour : MonoBehaviour {
             List<FluidParticle> foundNeighbours = new List<FluidParticle>();
             foreach (FluidParticle potentialNeighbour in Particles)
             {
-                if (potentialNeighbour != p && Vector3.Distance(potentialNeighbour.Position, p.Position) < NEIGHBOUR_DISTANCE)
+                if (potentialNeighbour != p && Vector3.Distance(potentialNeighbour.Position, p.Position) < NeighbourDistance)
                 {
                     foundNeighbours.Add(potentialNeighbour);
                 }
@@ -106,7 +123,7 @@ public class FluidBehaviour : MonoBehaviour {
 
         // Refine the predicted positions by taking incompressibilty into account
         // Lines 8-19 from the algorithm in the paper
-        for (int iteration = 0; iteration < SOLVER_ITERATIONS; iteration++)
+        for (int iteration = 0; iteration < SolverIterations; iteration++)
         {
             // Find lambda for each particle
             // Lines 9-11 from the algorithm in the paper
@@ -115,7 +132,7 @@ public class FluidBehaviour : MonoBehaviour {
                 FluidParticle p = Particles[i];
                 List<FluidParticle> foundNeighbours = neighbours[i];
                 float pi = StdSPHDensityEstimator(p, foundNeighbours);
-                float c = pi / REST_DENSITY - 1;
+                float c = pi / RestDensity - 1;
                 float deltaCSum = DerivativeCSum(p, foundNeighbours);
                 lambdas[i] = -c / deltaCSum;
             }
@@ -131,9 +148,10 @@ public class FluidBehaviour : MonoBehaviour {
                 {
                     deltaPos[i] += (lambdas[i] + lambdas[neigbour.Index]) * Vector3.Normalize(p.PredictedPosition - neigbour.Position) * SpikyDerivative(p, neigbour);
                 }
-                deltaPos[i] = 1 / REST_DENSITY * deltaPos[i] ;
+                deltaPos[i] = 1 / RestDensity * deltaPos[i];
                 Vector3 newPosition = p.PredictedPosition + deltaPos[i];
-                if (Physics.Linecast(p.PredictedPosition, newPosition + LAYER_OFFSET * (newPosition - p.Position).normalized)) {
+                if (Physics.Linecast(p.PredictedPosition, newPosition + ParticleDistance * (newPosition - p.Position).normalized))
+                {
                     deltaPos[i] = Vector3.zero;
                 }
             }
@@ -153,8 +171,8 @@ public class FluidBehaviour : MonoBehaviour {
             p.Velocity = p.PredictedPosition - p.Position;
             p.Position = p.PredictedPosition;
             p.Expiry--;
-        }        
-	}
+        }
+    }
 
     private float StdSPHDensityEstimator(FluidParticle p, List<FluidParticle> neighbours)
     {
@@ -162,8 +180,8 @@ public class FluidBehaviour : MonoBehaviour {
         foreach (FluidParticle neighbour in neighbours)
         {
             // Use the Poly6 kernel
-            pi += (315f / 64 * Mathf.PI * Mathf.Pow(NEIGHBOUR_DISTANCE, 4))
-                * Mathf.Pow(Mathf.Pow(NEIGHBOUR_DISTANCE,2) - Mathf.Pow(Vector3.Distance(p.PredictedPosition, neighbour.PredictedPosition), 2), 3); 
+            pi += (315f / 64 * Mathf.PI * Mathf.Pow(NeighbourDistance, 4))
+                * Mathf.Pow(Mathf.Pow(NeighbourDistance, 2) - Mathf.Pow(Vector3.Distance(p.PredictedPosition, neighbour.PredictedPosition), 2), 3);
         }
         return pi;
     }
@@ -178,39 +196,43 @@ public class FluidBehaviour : MonoBehaviour {
             sum += Mathf.Pow(derivative, 2);
 
         }
-        return sum + RELAXATION_CONSTANT;
+        return sum + RelaxationConstant;
     }
 
     private float SpikyDerivative(FluidParticle p1, FluidParticle p2)
     {
-        return 45 * Mathf.Pow(NEIGHBOUR_DISTANCE, 6) / Mathf.PI
-                * Mathf.Pow(NEIGHBOUR_DISTANCE - Vector3.Distance(p1.PredictedPosition, p2.PredictedPosition), 2);
-        
+        return 45 * Mathf.Pow(NeighbourDistance, 6) / Mathf.PI
+                * Mathf.Pow(NeighbourDistance - Vector3.Distance(p1.PredictedPosition, p2.PredictedPosition), 2);
+
     }
 
-	void OnDrawGizmos () {
-		Gizmos.color = Color.red;
-		foreach (FluidParticle p in Particles) {
-			Gizmos.DrawSphere(p.Position, LAYER_OFFSET / 2);
-		}
-	}
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (FluidParticle p in Particles)
+        {
+            Gizmos.DrawSphere(p.Position, ParticleDistance / 2);
+        }
+    }
 
-	public void ShootFluidParticles (Transform transform) {
-		foreach(Vector3 v in ParticleShot) {
+    public void ShootFluidParticles(Transform transform)
+    {
+        foreach (Vector3 v in particleShot)
+        {
             FluidParticle p = new FluidParticle(
-				transform.TransformPoint(v + new Vector3(0 ,0, 0.5f)) + new Vector3(0, 0.5f, 0),
-                transform.forward * PARTICLE_VELOCITY_SCALING,
+                transform.TransformPoint(v + new Vector3(0, 0, 0.5f)) + new Vector3(0, 0.5f, 0),
+                transform.forward * ParticleVelocityScaling,
                 random);
-			NewParticles.Add(p);
-		}
-	}
+            newParticles.Add(p);
+        }
+    }
 
     public void ShootFluidParticle(Transform transform)
     {
         FluidParticle p = new FluidParticle(
             transform.TransformPoint(new Vector3(0, 0, 0.5f)) + new Vector3(0, 0.5f, 0),
-            transform.forward * PARTICLE_VELOCITY_SCALING,
+            transform.forward * ParticleVelocityScaling,
             random);
-        NewParticles.Add(p);
+        newParticles.Add(p);
     }
 }
--- Assets/Scripts/FluidShooterBehaviour.cs
+++ Assets/Scripts/FluidShooterBehaviour.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FluidShooterBehaviour : MonoBehaviour {
+public class FluidShooterBehaviour : MonoBehaviour
+{
 
-	public FluidBehaviour water;
+    public FluidBehaviour water;
     private bool shootStream = false;
 
-	// Update is called once per frame
-	void FixedUpdate() {
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         shootStream = !shootStream;
-		if (Input.GetButtonDown ("Fire1")) {
-			water.ShootFluidParticles(this.transform);
-		}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            water.ShootFluidParticles(this.transform);
+        }
         if (Input.GetButton("Fire2") && shootStream)
         {
             water.ShootFluidParticle(this.transform);
         }
-	}
+    }
 }
